15ff9a06cc913b7d02d3972ebe963bd1d17e3cec
Dan Cashion <dangcashion@gmail.com>
2020-06-17 11:39:22 -0500

    Sorting finished

--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -4,21 +4,40 @@ def merge(arrA, arrB):
     merged_arr = [0] * elements
 
     # Your code here
-    pass
+    merged_arr= []
+    merged_arr.extend(arrA)
+    merged_arr.extend(arrB)
 
     return merged_arr
 
 # TO-DO: implement the Merge Sort function below recursively
 def merge_sort(arr):
     # Your code here
-    if len(arr) <= 1:
-        return arr
-
-        left = merge_sort(arr[:len(arr)//2])
-        right = merge_sort(arr[len(arr)//2:])
-        arr = merge(left, right)
-
-
+    if len(arr) > 1: 
+        mid = len(arr) // 2 
+        L = arr[:mid]
+        R = arr[mid:]
+        merge_sort(L)
+        merge_sort(R)
+        i = j = k = 0
+        while i < len(L) and j < len(R): 
+            if L[i] < R[j]: 
+                arr[k] = L[i] 
+                i+= 1
+            else: 
+                arr[k] = R[j] 
+                j+= 1
+            k+= 1
+            
+        while i < len(L): 
+            arr[k] = L[i] 
+            i+= 1
+            k+= 1
+            
+        while j < len(R): 
+            arr[k] = R[j] 
+            j+= 1
+            k+= 1
     return arr
 
 # STRETCH: implement the recursive logic for merge sort in a way that doesn't 
